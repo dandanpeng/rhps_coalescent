8b8cfb63d4652c4c79ef0569f9a7d4fe85f2a279
dandan peng <pddlrj@gmail.com>
2023-05-03 22:48:13 -0700

    reorganize some downstream analysis functions

--- small_example/pheno_sim_1iter.R
+++ small_example/pheno_sim_1iter.R
@@ -1,6 +1,9 @@
+
+
 #Doc Edge, 6/13/18
 #Goal: Simulate phenotypic scores (possibly under selection) that are additive functions
 # of genotypes at many loci. 
+
 
 #Plan: simulate n_loci allele frequency trajectories.
 #to do this, select an effect size from normal dist with mean 0 and sd
@@ -24,7 +27,6 @@ curr.freqs <- numeric(0)
 #analogous integral, which is ln(.99) - ln(.01) ~= 4.595
 ser <- 1:(2*N-1)
 harm.const <- sum(1/ser[ser >= .01*2*N & ser <= .99*2*N])
-
 
 
 #Simulate and write n.loci allele-frequency trajectories. None of these should have fixed,
@@ -87,24 +89,29 @@ rm(post.sel.freqs)
 rm(pre.sel.freqs)
 
 
-mat.trajs <- matrixify.list.of.trajs(trajs)
+mat.trajs <- matrixify.list.of.trajs(trajs) #matrix of derived allele freq at each locus
 
-phen.traj <- as.numeric(2 * eff_sizes %*% t(mat.trajs))  
-pt.time <- seq(0, by = 1/(2*N), length.out = max(sapply(trajs, length)/2) )
+phen.traj <- as.numeric(2 * eff_sizes %*% t(mat.trajs))  #phenotypes vector
+pt.time <- seq(0, by = 1/(2*N), length.out = max(sapply(trajs, length)/2) ) #desired time interval
 
 
 
 
 n_ders <- numeric(n.loci)
+
 ms_trees_list <- list()
 rent_trees_list <- list()
 relate_trees_list <- list()
+tsinfer_trees_list <- list()
+argweaver_samples_list <- list()
+argweaver_trees_list <- list()
 ms_haplotypes_list <- list()
 
 
 #for each locus, write trajectory, run ms to simulate a sample and tree,
 #and run rent+ to infer tree. Save both the ms and the rent+ trees, as well
 #as the number of derived alleles for each locus.
+
 for(i in 1:n.loci){
 	write.traj(traj.fn, trajs[[i]])
 	curr.freq.der <- curr.freqs[i]
@@ -112,58 +119,108 @@ for(i in 1:n.loci){
 	if(n_der == 0){n_der <- n_der + 1}
 	if(n_der == n_chroms){n_der <- n_der - 1}
 	n_ders[i] <- n_der
-	#run mssel
-	ms.string <- paste(ms_dir, "mssel ", as.character(n_chroms), " 1 ", as.character(n_chroms - n_der), " ", as.character(n_der), " ", traj.fn, " ", sel_site, " -r ", as.character(4*N*(1 - dbinom(0,len_hap, r))), " ", as.character(len_hap), " -t ", as.character(4*N* (1 - dbinom(0, len_hap, u))), " -T -L > ", msout.fn, sep = "")
-	system(ms.string)
-
-	#cut down mssel output to just the part rent+ needs
-	process.string <- paste("sed '/positions: /,$!d' ", msout.fn, " | sed 's/.*positions: //' > ", rent_in_fn, sep = "")
-	system(process.string)
-
-	#run rent+
-	rent.string <- paste("java -jar ", rentplus_fn, " -t -l ", as.character(len_hap), " ", rent_in_fn, " ", msout.fn, sep = "")
-	system(rent.string)
-
-	ms_haplotypes_list[[i]] <- readLines(rent_in_fn) #save the haplotypes produced by ms (and fed to rent+) in a list entry.
-	#the result will be a character vector. The first entry in the vector is relative (0-1) positions, with the selected
-	#site at 0.05. The remaining entries are string haplotypes of 0s and 1s.
 	
-	#run RELATE
-	source("relate_input.R")
-	relate.string <- paste("../../relate 
-	                       --mode All
-	                       -m", as.character(u),
-	                       "-N", as.character(N*2),
-	                       "--haps", relate_haps,
-	                       "--sample", relate_sample,
-	                       "--map", relate_map,
-	                       "--output", relate_out)
-  system(relate.string)
-  
-	#Read in trees at selected site in rent+
-	rent_trees_fn <- paste(rent_in_fn, ".trees", sep = "")
-	rent_trees <- readLines(rent_trees_fn)
-	#which tree is for the selected site?
-	rent_sel_ind <- grep(paste(as.character(sel_site), "\t", sep = ""), rent_trees)
+	
+	counter = 0
+	success = 1 # when RENT+ running fails
+	
+	while (success == 1 & counter <= 100){
+    	#run mssel
+    	ms.string <- paste(ms_dir, "mssel ", as.character(n_chroms), " 1 ", as.character(n_chroms - n_der), " ", as.character(n_der), " ", traj.fn, " ", sel_site, " -r ", as.character(4*N*(1 - dbinom(0,len_hap, re))), " ", as.character(len_hap), " -t ", as.character(4*N* (1 - dbinom(0, len_hap, u))), " -T -L > ", msout.fn, sep = "")
+    	system(ms.string)
+    
+        ######################## format processing #############################
+        
+    	#cut down mssel output to just the part rent+ needs
+    	process.string <- paste("sed '/positions: /,$!d' ", msout.fn, " | sed 's/.*positions: //' > ", rent_in_fn, sep = "")
+    	system(process.string)
+	
+      #run rent+
+      rent.string <- paste("java -jar ", rentplus_fn, " -t -l ", as.character(len_hap), " ", rent_in_fn, " ", msout.fn, sep = "")
+      success = system(rent.string)
+      counter = counter + 1
+    }
+	    
+    #transfer rent+ input to RELATE recognized format
+    source("relate_input.R")
 
-	#get tree at selected site in newick format, both true and rent-estimated.
-	rent_sel_tree_newick <- sub(paste(as.character(sel_site), "\t", sep = ""), "", rent_trees[rent_sel_ind])
-	rent_trees_list[[i]] <- read.tree(text = paste(rent_sel_tree_newick, ";", sep = ""))
-	
-	#Read in trees at selected site in RELATE
-	extract_tree <- paste("../../RelateExtract --mode AncToNewick",
-	                      "--anc data.anc", 
-	                      "--mut", as.character(u),
-	                      "--bp_of_interest", as.character(sel_site),
-	                      "--output", relate_out)
-	system(relate.string)
-	
-	relate_trees_list[[i]] <- read.tree(text = readLines(paste(relate_out, ".newick", sep = "")))
-  
-	for(k in 1:length(relate_trees_list[[i]]$tip.label)){
-	  relate_trees_list[[i]]$tip.label[k] <- as.character(as.numeric(relate_trees_list[[i]]$tip.label[k]) + 1)
-	}
-	
+	  #transfer rent+ input to argweaver recognized format
+	  #use_python("")
+    aw_input("rent_in.txt", len_hap, temp)   
+
+
+    ms_haplotypes_list[[i]] <- readLines(rent_in_fn) #save the haplotypes produced by ms (and fed to rent+) in a list entry.
+    #the result will be a character vector. The first entry in the vector is relative (0-1) positions, with the selected
+    #site at 0.05. The remaining entries are string haplotypes of 0s and 1s.
+    
+    ######################## run softwares #################################
+    #run RELATE
+    setwd(temp)
+    relate.string <- paste("../relate/Relate --mode All -m", as.character(u),
+                           "-N", as.character(N*2),
+                           "--haps data.haps", 
+                           "--sample data.sample",
+                           "--map data.map",
+                           "--output data")
+    system(relate.string)
+    setwd("..")
+    
+    #run tsinfer
+    source("TSinfer.R", verbose = TRUE)
+    nwtree = divide_newick(nwtree,4*N) # 4N gives the resulting tree a height of approx. 2
+     
+    #run ARGweaver
+    argweaver.string <- paste("arg-sample", "-s", argweaver_in_fn, "-N", N, "-r", re, "-m", u, "-n", as.character(aw_sample * 10), "--overwrite -o", argweaver_out_fn)
+    system(argweaver.string)
+    
+    ############################# extract the local tree ##########################
+
+    #Read in trees at selected site in rent+
+    rent_trees_fn <- paste(rent_in_fn, ".trees", sep = "")
+    rent_trees <- readLines(rent_trees_fn)
+    #which tree is for the selected site?
+    rent_sel_ind <- grep(paste(as.character(sel_site), "\t", sep = ""), rent_trees)
+    
+    if(length(rent_sel_ind) > 1){
+        rent_sel_ind <- rent_sel_ind[1]
+    }
+    
+    rent_sel_tree_newick <- sub(paste(as.character(sel_site), "\t", sep = ""), "", rent_trees[rent_sel_ind])
+    rent_trees_list[[i]] <- read.tree(text = paste(rent_sel_tree_newick, ";", sep = ""))
+
+
+    #Read in trees at selected site in RELATE
+    setwd(temp)
+    relate_sel_tree <- paste("../relate/RelateExtract --mode AncToNewick",
+                          "--anc data.anc",
+                          "--mut data.mut",
+                          "--bp_of_interest 100000",
+                          "--output data")
+    print("execute relateextract")
+    system(relate_sel_tree)
+    relate_trees_list[[i]] <- read.tree(text = readLines("data.newick"))
+    setwd("..")
+
+    for(k in 1:length(relate_trees_list[[i]]$tip.label)){
+      relate_trees_list[[i]]$tip.label[k] <- as.character(as.numeric(relate_trees_list[[i]]$tip.label[k]) + 1)
+    }
+
+    #Read in tree at selected site in tsinfer
+    tsinfer_sel_tree_newick <- nwtree
+    tsinfer_trees_list[[i]] <- read.tree(text = tsinfer_sel_tree_newick)
+
+    #Read in tree at selected site in ARGweaver
+    for(sample in 0:aw_sample){
+        aw.smc <- read.table(paste(argweaver_out_fn, ".", as.character(10*sample), ".smc.gz", sep = ""), skip = 2, header = FALSE, fill = TRUE)
+        sel_row <- aw.smc[aw.smc$V1 == "TREE" & aw.smc$V2 <= sel_site & aw.smc$V3 >= sel_site, ]
+        sel_tree <- read.tree(text = sel_row$V4)
+        ori_label <- sel_tree$tip.label
+        sel_tree$tip.label <- as.character(as.numeric(ori_label) + 1) # the original tip labels are zero-based numbering and we change them to one-based 
+        argweaver_samples_list[[sample + 1]] <- sel_tree
+    }
+    argweaver_trees_list[[i]] <- argweaver_samples_list
+
+    
 	#pull trees from ms
 	msoutlines <- readLines(msout.fn)
 	dists <- numeric(0)
@@ -175,159 +232,66 @@ for(i in 1:n.loci){
 	ms_to_extract <- which.max(posits.rs[posits.rs < sel_site]) + 1
 	if(length(ms_to_extract)  == 0){ms_to_extract <- 1}
 	ms_trees_list[[i]] <- read.tree(text = msoutlines[4 + ms_to_extract])
-  
 	
 	print(paste("simulation locus", as.character(i), "of trait", as.character(k), "complete"))
 }
-
 
 #Run through list and check for sites where 2+ variants had same coordinates as
 #selected site. At those places, check which tree(s) are monophyletic for derived tips.
 #if more than one, select randomly from among them.
 
-for(i in 1:n.loci){
-	if(is.null(names(ms_trees_list[[i]]))){
-		cand.trees <- ms_trees_list[[i]]
-		is.mono <- numeric(0)		
-		for(j in 1:length(cand.trees)){
-			der_tips <- which(cand.trees[[j]]$tip.label %in% as.character((n_chroms - n_ders[i] + 1):n_chroms))
-			is.mono[j] <- is.monophyletic(cand.trees[[j]], der_tips)
-		}
-		if(mean(is.mono) > 0){
-			ind.to.take <- 	sample(which(is.mono == 1), 1)
-		}		
-		if(mean(is.mono) == 0){ #if none are monophyletic, choose at random
-			ind.to.take <- sample(1:length(cand.trees), 1)
-		}
-		ms_trees_list[[i]] <- cand.trees[[ind.to.take]]
-		#rent_trees_list[[i]] <- rent_trees_list[[i]][[ind.to.take]]
-	}
-}
+check_samecor_sel_site(ms_trees_list)
+check_samecor_sel_site(rent_trees_list)
+check_samecor_sel_site(relate_trees_list)
+check_samecor_sel_site(tsinfer_trees_list)
+aw_check_samecor_sel_site(argweaver_trees_list)
+
+##rescale branch length of relate trees
+tmrca.relate <- numeric(0)
 
 for(i in 1:n.loci){
-	if(is.null(names(rent_trees_list[[i]]))){
-		cand.trees <- rent_trees_list[[i]]
-		is.mono <- numeric(0)		
-		for(j in 1:length(cand.trees)){
-			der_tips <- which(cand.trees[[j]]$tip.label %in% as.character((n_chroms - n_ders[i] + 1):n_chroms))
-			is.mono[j] <- is.monophyletic(cand.trees[[j]], der_tips)
-		}
-		if(mean(is.mono) > 0){
-			ind.to.take <- 	sample(which(is.mono == 1), 1)
-		}		
-		if(mean(is.mono) == 0){ #if none are monophyletic, choose at random
-			ind.to.take <- sample(1:length(cand.trees), 1)
-		}
-		#ms_trees_list[[i]] <- cand.trees[[ind.to.take]]
-		rent_trees_list[[i]] <- rent_trees_list[[i]][[ind.to.take]]
-	}
+  tmrca.relate[i] <- max(branching.times(relate_trees_list[[i]]))
 }
 
+
 for(i in 1:n.loci){
-  if(is.null(names(relate_trees_list[[i]]))){
-    cand.trees <- relate_trees_list[[i]]
-    is.mono <- numeric(0)		
-    for(j in 1:length(cand.trees)){
-      der_tips <- which(cand.trees[[j]]$tip.label %in% as.character((n_chroms - n_ders[i] + 1):n_chroms))
-      is.mono[j] <- is.monophyletic(cand.trees[[j]], der_tips)
+  relate_trees_list[[i]]$edge.length <- relate_trees_list[[i]]$edge.length*2/mean(tmrca.relate)
+}
+
+##rescale branch length of argweaver trees
+##The original branch lengths are given in units of generations.
+for(i in 1:n.loci){
+    for(j in 1:length(argweaver_trees_list[[i]])){
+        argweaver_trees_list[[i]][[j]]$edge
+        argweaver_trees_list[[i]][[j]]$edge.length <- argweaver_trees_list[[i]][[j]]$edge.length/(2*N)
     }
-    if(mean(is.mono) > 0){
-      ind.to.take <- 	sample(which(is.mono == 1), 1)
-    }		
-    if(mean(is.mono) == 0){ #if none are monophyletic, choose at random
-      ind.to.take <- sample(1:length(cand.trees), 1)
-    }
-    #ms_trees_list[[i]] <- cand.trees[[ind.to.take]]
-    relate_trees_list[[i]] <- relate_trees_list[[i]][[ind.to.take]]
-  }
 }
-
 #split into ancestral and derived trees and retrieve coalescence times.
-anc_trees_ms <- list()
-der_trees_ms <- list()
-ct_ms_all <- list()
-ct_ms_anc <- list()
-ct_ms_der <- list()
 
-anc_trees_rent <- list()
-der_trees_rent <- list()
-ct_rent_all <- list()
-ct_rent_anc <- list()
-ct_rent_der <- list()
+split_tree("ms", ms_trees_list)
+split_tree("rent", rent_trees_list)
+split_tree("relate", relate_trees_list)
+split_tree("tsinfer", tsinfer_trees_list)
+aw_split_tree("argweaver", argweaver_trees_list)
 
-anc_trees_relate <- list()
-der_trees_relate <- list()
-ct_relate_all <- list()
-ct_relate_anc <- list()
-ct_relate_der <- list()
+#store coalesenct times of the whole tree, anc tree and der tree
+coal_time_ls(ms_trees_list, anc_trees_ms, der_trees_ms, "ms")
+coal_time_ls(rent_trees_list, anc_trees_rent, der_trees_rent, "rent")
+coal_time_ls(relate_trees_list, anc_trees_relate, der_trees_relate, "relate")
+coal_time_ls(tsinfer_trees_list, anc_trees_tsinfer, der_trees_tsinfer, "tsinfer")
+aw_coal_time_ls(argweaver_trees_list, anc_trees_argweaver, der_trees_argweaver)
 
-for(i in 1:n.loci){
-	anc_tips_rent <- which(rent_trees_list[[i]]$tip.label %in% as.character(1:(n_chroms - n_ders[i])))
-	anc_tips_ms <- which(ms_trees_list[[i]]$tip.label %in% as.character(1:(n_chroms - n_ders[i])))
-	anc_tips_relate <- which(relate_trees_list[[i]]$tip.label %in% as.character(1:(n_chroms - n_ders[i])))
-	
-	der_tips_rent <- which(rent_trees_list[[i]]$tip.label %in% as.character((n_chroms - n_ders[i] + 1):n_chroms))
-	der_tips_ms <- which(ms_trees_list[[i]]$tip.label %in% as.character((n_chroms - n_ders[i] + 1):n_chroms))
-	der_tips_relate <- which(relate_trees_list[[i]]$tip.label %in% as.character((n_chroms - n_ders[i] + 1):n_chroms))
-	
-	anc_trees_rent[[i]] <- drop.tip(rent_trees_list[[i]], der_tips_rent)
-	der_trees_rent[[i]] <- drop.tip(rent_trees_list[[i]], anc_tips_rent)
-	anc_trees_ms[[i]] <- drop.tip(ms_trees_list[[i]], der_tips_ms)
-	der_trees_ms[[i]] <- drop.tip(ms_trees_list[[i]], anc_tips_ms)
-	anc_trees_relate[[i]] <- drop.tip(relate_trees_list[[i]], der_tips_relate)
-	der_trees_relate[[i]] <- drop.tip(relate_trees_list[[i]], anc_tips_relate)
-	
-	ct_rent_all[[i]] <- coal.times(rent_trees_list[[i]])
-	ct_rent_anc[[i]] <- coal.times(anc_trees_rent[[i]])
-	ct_rent_der[[i]] <- coal.times(der_trees_rent[[i]])
-	ct_ms_all[[i]] <- coal.times(ms_trees_list[[i]])
-	ct_ms_anc[[i]] <- coal.times(anc_trees_ms[[i]])
-	ct_ms_der[[i]] <- coal.times(der_trees_ms[[i]])
-	ct_relate_all[[i]] <- coal.times(relate_trees_list[[i]])
-	ct_relate_anc[[i]] <- coal.times(anc_trees_relate[[i]])
-  ct_relate_der[[i]] <- coal.times(der_trees_relate[[i]])	
-	print(paste("round", as.character(i), "of tree processing complete"))
-}
-
-
-
-
-times.c <- list()
-for(i in 1:length(ms_trees_list)){
-	times.c[[i]] <- trees_to_times(ms_trees_list[[i]], anc_trees_ms[[i]], der_trees_ms[[i]], time, sure.alt.is.derived = FALSE, units_in = 4)
-}
-
-lins.list <- list()
-for(i in 1:length(ms_trees_list)){
-	lins.list[[i]] <- times_to_lins(times.c[[i]], time)
-}
-
-
-times.c.rent <- list()
-for(i in 1:length(rent_trees_list)){
-	times.c.rent[[i]] <- trees_to_times(rent_trees_list[[i]], anc_trees_rent[[i]], der_trees_rent[[i]], time, sure.alt.is.derived = FALSE, units_in = 2)
-}
-
-lins.list.rent <- list()
-for(i in 1:length(rent_trees_list)){
-	lins.list.rent[[i]] <- times_to_lins(times.c.rent[[i]], time)
-}
-
-
-times.c.relate <- list()
-for(i in 1:length(relate_trees_list)){
-  times.c.relate[[i]] <- trees_to_times(relate_trees_list[[i]], anc_trees_relate[[i]], der_trees_relate[[i]], time, sure.alt.is.derived = FALSE, units_in = 2)
-}
-
-lins.list.relqte <- list()
-for(i in 1:length(relate_trees_list)){
-  lins.list.relate[[i]] <- times_to_lins(times.c.relate[[i]], time)
-}
+# store the number of lineages of ref allele (col1) and alt allel (col2) into a list of matrix
+lins_ls(ms_trees_list, "ms")
+lins_ls(rent_trees_list, "rent")
+lins_ls(relate_trees_list, "relate")
+lins_ls(tsinfer_trees_list, "tsinfer")
+aw_lins_ls(argweaver_trees_list, times.c.argweaver)
 
 fn_str <- paste("loci", as.character(n.loci), "_sintens", as.character(round(sel.intens,3)), "_N", as.character(N), "_nchr", as.character(n_chroms), "_ton", as.character(t), "_toff", as.character(t.off), "_herit", as.character(herit), "_", as.character(phen_num), sep = "")
 
 
-save.image(paste(out_dir, fn_str, ".RData", sep = ""))
+save.image(paste(out_dir, "/", fn_str, ".RData", sep = ""))
 
 
 
